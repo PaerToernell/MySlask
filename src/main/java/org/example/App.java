b10f37d32043d1e5018246007d522980aeeb0078
Pär Törnell <paer@ijob.se>
2021-12-20 10:02:25 +0100

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String[] names = new String [15];
+        char[] myChars = new char[15];
+        names[1]="Anders";
+        System.out.println( "Hello World! "+names[1]+" "+names.length );
+    }
+}
